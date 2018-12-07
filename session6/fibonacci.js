b981f758ca2ec12997ce1d1002cec8fb886d09df
Quang Le <lquang@ESILICON.COM>
2018-12-07 18:39:58 +0700

    nop bai

--- session6/fibonacci.js
+++ session6/fibonacci.js
@@ -1,15 +1,11 @@
 var f1 = 1;
-var f2 = 1;
-var f3; 
+var f2 = 1; 
 var count = 0;
 document.write(f1," ",f2," ");
 function fibonacci ( f1 , f2 ) {
-	f3 = f1 + f2 ;
-	if (f3 <= 100) {
-		document.write(f3," ");	
-		f1 = f2;
-		f2 = f3;
-		fibonacci(f1,f2);
+	if (f1 + f2 <= 100) {
+		document.write(f1+f2," ");	
+		fibonacci(f2,f1+f2);
 	} else {
 		return;
 	}
---
+++ session7/ex1.js
@@ -0,0 +1,19 @@
+var a, b;
+a = 5;
+b = 6;
+function cong(a,b) {
+	document.write("Tong hai so la : ",a+b,"<br>");
+}
+function tru(a,b) {
+	document.write("Hieu hai so la : ",a-b,"<br>");
+}
+function nhan(a,b) {
+	document.write("Tich hai so la : ",a*b,"<br>");
+}
+function chia(a,b) {
+	document.write("Thuong hai so la : ",a/b,"<br>");
+}
+cong(a,b);
+tru(a,b);
+nhan(a,b);
+chia(a,b);
